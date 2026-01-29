b44f910f7710cf82373b8bc1fd49fae759262f06
Isak W <isakwarsame3@gmail.com>
2026-01-29 09:59:19 +0100

    Add sample controller

---
+++ Api/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1")]
+public class SampleController : ControllerBase
+{
+    [HttpGet("hello")]
+    public IActionResult GetHello()
+    {
+        return Ok("Hello from SampleController!");
+    }
+}
