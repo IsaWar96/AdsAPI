044296f4015c9419e57beabfaef45126590e150b
Arnell <Arnell.1996@hotmail.com>
2026-01-29 11:47:38 +0100

    Add Ad model with properties for Title, Description, ImgUrl, and HtmlUrl

---
+++ Api/Models/Ad.cs
@@ -0,0 +1,10 @@
+namespace Api.Models;
+
+public class Ad
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImgUrl { get; set; } = string.Empty;
+    public string HtmlUrl { get; set; } = string.Empty;
+
+}
